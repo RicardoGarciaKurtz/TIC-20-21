9b020cb5804389bd3427009b8df8d0acb38fabcb
RicardoGarciaKurtz <71324245+RicardoGarciaKurtz@users.noreply.github.com>
2020-10-05 17:03:36 +0200

    Add files via upload

---
+++ nombre.py
@@ -0,0 +1,9 @@
+
+'''Escribe un programa que genere contrasenas'''  
+def contrasena():
+    nombre=raw_input("introduce tu nombre: ")
+    apellido=raw_input("introduce tu apellido: ")
+    print nombre, "", apellido, "",
+    print "contrasena=",3*nombre
+contrasena()
+           
---
+++ ululador.py
@@ -0,0 +1,10 @@
+'''haz un programa que lea una cadena y sustituya
+las vocales por la letra u. palabra de 10, for '''
+def ululador():
+    palabra=raw_input("introduce una palabra para ulular. ")
+    for cont in range(0,100):
+        if(palabra[cont]=='a' OR palabra[cont]=='e' or palabra[cont]='i' or ):
+            print 'u'
+        else:
+            print palabra[cont]
+ululador()
