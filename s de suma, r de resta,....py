df9024f3b4c81f3e7b2411d6ab34f33daf6ba7f9
RicardoGarciaKurtz <71324245+RicardoGarciaKurtz@users.noreply.github.com>
2020-11-09 13:47:25 +0100

    Add files via upload

--- s de suma, r de resta,....py
+++ s de suma, r de resta,....py
@@ -6,22 +6,33 @@ m de multiplicacion y d de division
 def menu():
     print "introduce dos numeros enteros"
     numero1=input("numero 1= ")
-    numero2=input("numero 2= ")
-    print "************************"
-    print "*               menu                 *"
-    print "************************"
-    print "1. suma"
-    print "2. resta"
-    print "3. multiplicacion"
-    print "4. division"
-    opcion= input("teclee la opcion elegida ")
-    if(opcion==1):
-        resultado=numero1+numero2
-    if(opcion==2):
-        resultado=numero1-numero2
-    if(opcion==3):
-        resultado=numero1*numero2
-    if(opcion==4):
-        resultado=numero1/numero2
+    interruptora=1
+    while(interruptora==1):
+        numero2=input("numero 2= ")
+        print "************************"
+        print "*               menu                 *"
+        print "************************"
+        print "1. suma"
+        print "2. resta"
+        print "3. multiplicacion"
+        print "4. division"
+        opcion= input("teclee la opcion elegida ")
+        if(opcion==1):
+            resultado=numero1+numero2
+            interruptora=0
+        if(opcion==2):
+            resultado=numero1-numero2
+            interruptora=0
+        if(opcion==3):
+            resultado=numero1*numero2
+            interruptora=0
+        if(opcion==4):
+            if(numero2!=0):
+                interruptora=0
+                resultado=numero1/numero2
+            else:
+                interruptora=1
+                print "has intentado dividir entre 0 "
+                print "introduce un nuevo numero "
     print "el resultado es", resultado
 menu() 
--- ululador.py
+++ ululador.py
@@ -3,7 +3,7 @@ las vocales por la letra u. palabra de 10, for '''
 def ululador():
     palabra=raw_input("introduce una palabra para ulular. ")
     for cont in range(0,100):
-        if(palabra[cont]=='a' OR palabra[cont]=='e' or palabra[cont]='i' or ):
+        if(palabra[cont]=='a' or palabra[cont]=='e' or palabra[cont]=='i' or palabra[cont]=='o'):
             print 'u'
         else:
             print palabra[cont]
