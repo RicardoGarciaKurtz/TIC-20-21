9c3170678b32dcc5eb55d7496db2e140ea289351
RicardoGarciaKurtz <71324245+RicardoGarciaKurtz@users.noreply.github.com>
2021-02-18 12:20:29 +0100

    Add files via upload

---
+++ suspenso maybe.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+int main(){
+	int pepito[5];
+	int cont;
+	int salir;
+	int suma=0;
+	for(cont=0;cont<5;cont++){
+		cout<<"dime un numero: ";
+		cin>>pepito[cont];
+	}
+	cout<<" la suma vale: ";
+	for(cont=0,cont<5,cont++){
+		suma=cont+1
+	}
+
+}
